c2d310ac4881ce92b7caebd83d16e29a568ae8f1
Taxane <darian.richardson@gmail.com>
2019-12-30 16:02:31 -0600

    Start of cryptomsg

---
+++ cryptomsg.py
@@ -0,0 +1,18 @@
+#! /usr/bin/python3
+
+# Cryptomsg
+# by: Darian
+
+
+
+import random
+import math
+
+charlist = ['a', 'A', 'b', 'B', 'c', 'C', 'd', 'D', 'e', 'E', 'f', 'F', 'g','G', 'h', 'H', 'i', 'I', 'j', 'J', 'k', 'K', 'l', 'L', 'm', 'M', 'n', 'N', 'o', 'O', 'p', 'P', 'q', 'Q', 'r', 'R', 's', 'S', 't', 'T', 'u', 'U', 'v', 'V', 'w', 'W', 'x', 'x', 'y', 'Y', 'z', 'Z', '.', ',', '?', '/', '\\', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '+', '+', "'", '"', ':', ';', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+
+
+def otpgen(msglen=1024, participants='DD', numOfFiles=10):
+	for i in range(numOfFiles):
+		
+		for i in range(msglen)
+	
